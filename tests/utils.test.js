5134c340ef519b9addad63c21de8c0a17864276e
kailykins <kaily.newman@infinityworks.com>
2021-10-13 15:25:48 +0100

    tested reverse array function

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -22,3 +22,26 @@ describe("build Sequential Array function", () => {
     });
   });
 });
+
+describe("reverse Array function", () => {
+  it("reverses an array with 3 elements from 0 to 2", () => {
+    const array = [0, 1, 2];
+    const expected = [2, 1, 0];
+    const actual = utils.reverseArray(array);
+    expect(actual).toEqual(expected);
+  });
+  it("reverses an array with 5 elements from 0 to 4", () => {
+    const array = [0, 1, 2, 3, 4];
+    const expected = [4, 3, 2, 1, 0];
+    const actual = utils.reverseArray(array);
+    expect(actual).toEqual(expected);
+  });
+  describe("unhappy path", () => {
+    it("returns an empty array for length 0", () => {
+      const array = [];
+      const expected = [];
+      const actual = utils.reverseArray(array);
+      expect(actual).toEqual(expected);
+    });
+  });
+});
